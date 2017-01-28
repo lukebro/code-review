77c8593450f307fd7ded75b499cde82985ff8933
Lukasz Brodowski <lukebrodowski@gmail.com>
2017-01-28 15:36:44 -0500

    Home page styling

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -6,13 +6,37 @@
   <div class="hero-body">
     <div class="container">
       <h1 class="title">
-        Code Review
+        The bridge between coding and teaching.
       </h1>
       <h2 class="subtitle">
-        Blah blah blah.
+        Code review between students and teachers.
       </h2>
+      <p><a href="{{ route('login') }}" class="button is-primary is-inverted is-outlined is-medium"><span class="icon is-medium"><i class="fa fa-github"></i></span><span>Sign up through GitHub</span></a></p>
     </div>
   </div>
+</section>
+
+<section class="section">
+  <nav class="level">
+  <div class="level-item has-text-centered">
+    <div>
+      <p class="heading">Student</p>
+      <p class="title">3,456</p>
+    </div>
+  </div>
+  <div class="level-item has-text-centered">
+    <div>
+      <p class="heading">Teachers</p>
+      <p class="title">123</p>
+    </div>
+  </div>
+  <div class="level-item has-text-centered">
+    <div>
+      <p class="heading">Assignements</p>
+      <p class="title">456</p>
+    </div>
+  </div>
+</nav>
 </section>
 
 @endsection
